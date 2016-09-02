80c508a5e6ecb4c3300b05a17e7b17571e529be5
Ferdinand Leinbach <ferdi.leinbach@gmail.com>
2016-09-02 19:32:56 +0200

    corrected github link in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'A wrapper around matplotlib plot for parametric pandas dataframe experiment data',
   author = 'Ferdinand Leinbach',
   author_email = 'ferid.leinbach@googlemail.com',
-  url = 'https://github.com/effibeast/dfparaplot', # use the URL to the github repo
-  download_url = 'https://github.com/effibeast/dfparaplot/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/effi/dfparaplot', # use the URL to the github repo
+  download_url = 'https://github.com/effi/dfparaplot/tarball/0.1', # I'll explain this in a second
   keywords = ['ploting', 'parametric', 'dataframe'], # arbitrary keywords
   classifiers = [],
 )
